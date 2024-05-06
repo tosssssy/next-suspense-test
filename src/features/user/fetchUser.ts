92bc7d210598d3aca74407c2d7d094e41e3125e4
tosssssy <t.kabu.03@gmail.com>
2024-05-06 18:02:24 +0900

    fetch関数を使う

--- src/features/user/fetchUser.ts
+++ src/features/user/fetchUser.ts
@@ -1,8 +1,18 @@
 export const fetchUser = async (type: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users?id=3",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const user = await response.json();
 
   return {
-    name: "ユーザー",
+    name: user[0].name,
     type,
   };
 };
